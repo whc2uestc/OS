8642a431b074eabe83554377e117f74034de2836
汪寒成 <171094685@qq.com>
2015-11-12 15:15:41 +0800

    the head file of config.

---
+++ Assembler/cfg.h
@@ -0,0 +1,31 @@
+#ifndef CFG_H_
+#define CFG_H_
+
+#define LOAD 0x60 
+#define LOADI 0x61 
+#define STORE 0x62 
+#define STOREI 0x63 
+#define JMP 0x64 
+#define JMPO 0x65 
+#define JMPZ 0x66 
+#define JMPN 0x67 
+#define ADD 0x68 
+#define ADDR 0x69 
+#define SUB 0x6a 
+#define SUBR 0x6b 
+#define OUTI 0x6c 
+#define OUTR 0x6d 
+#define OUTIC 0x6e 
+#define OUTRC 0x6f 
+#define OUT 0x70 
+#define OUTC 0x71 
+#define HALT 0x72
+
+#define A1 0
+#define A2 2
+#define R1 3
+#define R2 1
+
+
+
+#endif
